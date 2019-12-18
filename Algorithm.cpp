04fb5908f3c97801b4717a889e52f818ea086934
Alceec <33930767+Alceec@users.noreply.github.com>
2019-12-18 10:19:41 +0100

    Update Algorithm.cpp

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,155 +1,65 @@
 #include "Algorithm.h"
-#define max(x,y) ( x < y ? y : x ) //
+#define max(x,y) ( x < y ? y : x )
 
-Algorithm::Algorithm(int R, int Q){
-	penalExtension = R; // R
-	penalOpen = Q; // Q
+Algorithm::Algorithm(int R, int Q, ofstream * pfichier){
+	
+	penalExtension = R;
+	penalOpen = Q;
+
+	// écrire les informations de l'algorithme dans le fichier texte
+	if (pfichier->is_open()){
+		*pfichier << "ALGORITHM INFORMATION :" << endl;
+		*pfichier << "Score matrix = BLOSSUM" << endl;
+		*pfichier << "Extension Penalty = " << R << endl;
+		*pfichier << "Open Penalty = " << Q << endl << endl;
+	}
 }
 
-int Algorithm::calculateScore(vector<int> querySequence, int sequenceSize, int sequenceSize2, int m_matrixBlosum[28][28], int * databaseSequence){
+int Algorithm::calculateScore(int * querySequence, int sequenceSize, int sequenceSize2, int m_matrixBlosum[28][28], int * databaseSequence){
+	
+	// calculer le score d'un alignement entre deux séquences, basé sur la matrice blosum
 	
 	int* matrixH = new int[sequenceSize+1];
 	int* matrixE = new int[sequenceSize+1];
 	int F = 0;
-	int Hhaut = 0;
-	int Hdiag = 0;
-	int Hmaintenant = 0;
+	int H_up = 0;
+	int H_diag = 0;
+	int H = 0;
+	
 	int scoreMax =0;
 	
 	for (unsigned int i=0; i< sequenceSize+1;i++)
 		{
 		matrixE[i] = 0;
 		matrixH[i] = 0;
-		
 		}
 
 	for (unsigned int j=1; j<sequenceSize2+1; j++) { 
 		for (unsigned int i=1; i<sequenceSize+1; i++) { 
+			
 			matrixE[i]=max(matrixH[i]-penalOpen,matrixE[i]-penalExtension);
-			F=max(Hhaut-penalOpen, F-penalExtension);
-			Hdiag = matrixH[i-1] + m_matrixBlosum[databaseSequence[j-1]][querySequence[i-1]];
-			Hmaintenant = max(Hdiag, matrixE[i]);
-			Hmaintenant = max(Hmaintenant, F);
-			Hmaintenant = max(Hmaintenant, 0);
-
-			if(Hmaintenant > scoreMax){
-				scoreMax = Hmaintenant;
-			}
-			matrixH[i-1] = Hhaut;
-			Hhaut= Hmaintenant;
-		}
-	
-		matrixH[sequenceSize] = Hmaintenant;
-		
-		Hhaut = 0;
-	}
-	
-	delete[] matrixH;
-	delete[] matrixE;
-	delete[] databaseSequence;
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	int* matrixH = new int[sequenceSize+1];
-	int* matrixE = new int[sequenceSize+1];
-	int F = 0;
-	int Hhaut = 0;
-	int Hdiag = 0;
-	int Hmaintenant = 0;
-	int scoreMax = 0;
-
-	for (unsigned int i=0; i< sequenceSize+1;i++) {
-		matrixE[i] = 0;
-		matrixH[i] = 0;
-	}
-
-	for (unsigned int j=1; j<sequenceSize2+1; j++) {
-		for (unsigned int i=1; i<sequenceSize+1; i++) {  
-			matrixE[i]=max(matrixH[i]-penalOpen,matrixE[i]-penalExtension);
-			F=max(Hhaut-penalOpen, F-penalExtension);
-			Hdiag = matrixH[i-1] + m_matrixBlosum[databaseSequence[j-1]][querySequence[i-1]];
-			Hmaintenant = max(Hdiag, matrixE[i]);
-			Hmaintenant = max(Hmaintenant, F);
-			Hmaintenant = max(Hmaintenant, 0);
-
-			if(Hmaintenant > scoreMax){
-				scoreMax = Hmaintenant;
-			}
-			matrixH[i-1] = Hhaut;
-			Hhaut= Hmaintenant;
-		}
-	
-		matrixH[sequenceSize] = Hmaintenant;
-		
-		Hhaut = 0;
-	}
-	delete[] matrixH;
-	delete[] matrixE;
-	delete[] databaseSequence;//
-	
-	/*
-	// impression de la séquence trouvée dans le sequence file
-	cout << "Séquence trouvée dans la database = ";
-	for (int n:foundSequence){
-		cout << n;
-	} cout << endl;*/
-	
-	/*
-	// créer les matrice H, E et F
-	int* matrixH = new int[sequenceSize*sequenceSize2];
-	int* matrixE = new int[sequenceSize*sequenceSize2];
-	int* matrixF = new int[sequenceSize*sequenceSize2];
-	for (unsigned int i=0; i< sequenceSize*sequenceSize2;i++) {
-		matrixE[i] = 0;
-		matrixF[i] = 0;
-		matrixH[i] = 0;
-		}		
-	// calculer les scores dans la matrice H avec les matrices E et F
-	for (unsigned int i=1; i<sequenceSize; i++) {
-		for (unsigned int j=1; j<sequenceSize2; j++) {
-			//Eij
-			if(matrixE[i*sequenceSize2 + j-1] - penalExtension < matrixH[i*sequenceSize2 + j-1] - penalOpen ) {		
-				matrixE[i*sequenceSize2 + j] = matrixH[i*sequenceSize2 + j-1] - penalOpen;
-			}
-			else {
-				matrixE[i*sequenceSize2 + j] = matrixE[i*sequenceSize2 + j-1] - penalExtension;
-			}
-			//FIJ 
-			if(matrixF[(i-1)*sequenceSize2 + j] - penalExtension < matrixH[(i-1)*sequenceSize2 + j] - penalOpen ) {		
-				matrixF[i*sequenceSize2 + j] = matrixH[(i-1)*sequenceSize2 + j] - penalOpen;
-			}
-			else {
-				matrixF[i*sequenceSize2 + j] = matrixF[(i-1)*sequenceSize2 + j] - penalExtension;
-			}
-			//Hij
-			matrixH[i*sequenceSize2 + j] = matrixH[(i-1)*sequenceSize2+j-1] + m_matrixBlosum[querySequence[i-1]][databaseSequence[j-1]];
-			if(matrixH[i*sequenceSize2 + j] < (matrixE[i*sequenceSize2 + j])) {
-				matrixH[i*sequenceSize2 + j] = (matrixE[i*sequenceSize2 + j]);
-			}
-			if(matrixH[i*sequenceSize2 + j] < (matrixF[i*sequenceSize2 + j])) {
-				matrixH[i*sequenceSize2 + j] = (matrixF[i*sequenceSize2 + j]);
-			}
-			if(matrixH[i*sequenceSize2 + j] < 0) {
-				matrixH[i*sequenceSize2 + j] = 0;
+			F=max(H_up-penalOpen, F-penalExtension);
+			H_diag = matrixH[i-1] + m_matrixBlosum[databaseSequence[j-1]][querySequence[i-1]];
+			H = max(H_diag, matrixE[i]);
+			H = max(H, F);
+			H = max(H, 0);
+			
+			if(H > scoreMax){
+				scoreMax = H;
 			}
 			
-			// chercher le score maximum (= meilleur alignement possible) dans la matrice H
-			if (matrixH[i*sequenceSize2 + j] > scoreMax){
-				scoreMax = matrixH[i*sequenceSize2 + j];
-			};
-		}	
+			matrixH[i-1] = H_up;
+			H_up= H;
+		}
+	
+		matrixH[sequenceSize] = H;
+		
+		H_up = 0;
 	}
 	
-	delete matrixH;
-	delete matrixE;
-	delete matrixF;
-	*/
+	delete[] matrixH;
+	delete[] matrixE;
+	//delete[] databaseSequence;
 	
 	return scoreMax;
 	
@@ -157,53 +67,61 @@ int Algorithm::calculateScore(vector<int> querySequence, int sequenceSize, int s
 
 void Algorithm::calculate(Sequence * sequence, Database * database, int m_matrixBlosum[28][28]){
 	
-	// ouvrir le sequence file
-	ifstream sequenceFile;
-	sequenceFile.open("./newE.fasta.psq");
-	
 	// prendre les informations utiles de la database et de la query sequence
-	vector<int> querySequence = sequence->getSequence(); 
+	int * querySequence = sequence->getSequence(); 
 	int querySequenceSize = sequence->getSize(); 
 	vector<uint32_t> sequenceOffsetTable = database->getSequenceOffsetTable(); 
-	vector<uint32_t> headerOffsetTable = database->getHeaderOffsetTable(); 
+	vector<uint32_t> headerOffsetTable = database->getHeaderOffsetTable();
 	
-	int results[5] = {0};
-	int scores[5] = {0};
+	// ouvrir le sequence file
+	ifstream sequenceFile;
+	sequenceFile.open(database->getSequenceFileName());
 	
-	// parcourir toutes les séquences de la database, calculer leur score et garder les 5 meilleurs scores
-	for (unsigned int p = 0; p<sequenceOffsetTable.size()-1; p++){
-		sequenceFile.seekg(sequenceOffsetTable[p]);
-		int databaseSequenceSize = sequenceOffsetTable[p+1]-sequenceOffsetTable[p];
-		//int databaseSequence[databaseSequenceSize] = {0};//
-		int* databaseSequence = new int[databaseSequenceSize]; //
-		for (unsigned int i=0; i<databaseSequenceSize; i++){
-			char x;
-			if (sequenceFile.get(x)){
-				databaseSequence[i] = (x);
+	// initialiser les tableaux pour enregistrer les 50 meilleurs scores
+	int results[50] = {0};
+	int scores[50] = {0};
+	
+	// parcourir toutes les séquences de la database, calculer leur score et garder les 50 meilleurs scores
+	if (sequenceFile.is_open()){
+		for (unsigned int p = 0; p<sequenceOffsetTable.size()-1; p++){
+			sequenceFile.seekg(sequenceOffsetTable[p]);
+			int databaseSequenceSize = sequenceOffsetTable[p+1]-sequenceOffsetTable[p];
+			int* databaseSequence = new int[databaseSequenceSize];
+			for (unsigned int i=0; i<databaseSequenceSize; i++){
+				char x;
+				if (sequenceFile.get(x)){
+					databaseSequence[i] = (x);
+				}
 			}
-		} 
-		int score = calculateScore(querySequence, querySequenceSize, databaseSequenceSize, m_matrixBlosum, databaseSequence);
-		// regarder si la séquence considérée a un meilleur alignement (meilleur score) que celles déjà retenues
-		
-		
-		if (score > scores[0]){
-			scores[0] = score;
-			results[0] = p;
-			int i = 0;
-			while (scores[i] > scores[i+1]){
-				int temp = scores[i];
-				scores[i] = scores[i+1];
-				scores[i+1] = temp;
-				int temp2 = results[i];
-				results[i] = results[i+1];
-				results[i+1] = temp2;
-				i++;
-				if (i == 4){
-					break;
+			
+			int score = calculateScore(querySequence, querySequenceSize, databaseSequenceSize, m_matrixBlosum, databaseSequence);
+			
+			delete[] databaseSequence;
+			
+			// regarder si la séquence considérée a un meilleur alignement (meilleur score) que celles déjà retenues
+			
+			if (score > scores[0]){
+				scores[0] = score;
+				results[0] = p;
+				int i = 0;
+				while (scores[i] > scores[i+1]){
+					int temp = scores[i];
+					scores[i] = scores[i+1];
+					scores[i+1] = temp;
+					int temp2 = results[i];
+					results[i] = results[i+1];
+					results[i+1] = temp2;
+					i++;
+					if (i == 49){
+						break;
+					}
 				}
 			}
 		}
+	} else {
+		cout << "You do not have the right sequence file name. The name of the file should be " << database->getSequenceFileName() << endl;
 	}
 	sequenceFile.close();
+	// imprimer les résultats
 	database->printSequenceName(results, scores);
 }
